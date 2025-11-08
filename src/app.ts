01ed6ff104736a5ec6cb05f4d83e2e99d8c20a04
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-11-08 20:59:44 +0800

    chore: setup for esbuild

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+let statusNode = querySelector("#status")
+
+statusNode.textContent = "Ready"
+
+function querySelector<E extends HTMLElement>(selector: string) {
+  let element = document.querySelector<E>(selector)
+  if (!element) {
+    throw new Error(`Element not found: "${selector}"`)
+  }
+  return element
+}
