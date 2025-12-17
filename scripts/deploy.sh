0040c0bd3be6b7ba60f6d8efe2879e4de8964d8f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-12-18 05:04:16 +0800

    chore: move index.html and bundle.js to a subfolder to simplify deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,4 @@
 set -euo pipefail
 
 npm run build
-
-rm -rf public
-mkdir public
-cp index.html bundle.js public/
-
-npx surge public https://audio-spectrogram.surge.sh
-
-rm -rf public
+npx surge web
